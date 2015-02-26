c2db9a5c2629e0153051eb8223fbfc2b9d2c6c9c
lidiamcfreitas <lidiamcfreitas@gmail.com>
2015-02-26 11:32:24 +0000

    added working version that reads from file

--- simulator.py
+++ simulator.py
@@ -1,33 +1,48 @@
 import random
-
-
+"""
 maxnodes = 4
 experiments =2
 runsPerExp = 10
 roundsPerRun = 10
 maxnodeval = 200
+"""
 
 nodes = []
 
+
 def dumpstates():
+    """ Prints the nodes representation."""
     string = ""
     for i in range(len(nodes)):
         print("----Node[%d].my_val=%d;" % (i,nodes[i].value))
+        
+
+def readvalues(filename):
+    """reads input configuration from filename"""
+    file = open(filename,"r")
+    for line in file:
+        results = line[:-1].split(",")
+        results = tuple([int(elem) for elem in results])
+    return results
 
 def selectRandomInt(maxvalue):
+    """ Selects a random value between 0 and (maxvalue-1)."""
     return int(random.random()*maxvalue)
 
 def selectRandomNode():
+    """ Selects a random node by returning it's index."""
     return selectRandomInt(maxnodes)
 
 def initnodes(algorithm):
+    """ Initializes the nodes given an algorithm."""
     return algorithm.execute()
 
 def getmax(i,j):
+    """ Returns the maximum value of i and j."""
     return (i,j)[i<j] # true - 1; false - 0
 
 def assertAlgo(maxInNetwork):
-    
+    """Verifies the value of the nodes to equal maxInNetwork. 0-incorrect.1-correct."""
     if (maxInNetwork>0):
         for node in nodes:
             if (node.value != maxInNetwork):
@@ -36,28 +51,30 @@ def assertAlgo(maxInNetwork):
     return 0
 
 def encounter( i, j):
+    """Specifies the algorithm to use when two nodes collide."""
     nodes[i].value = getmax(nodes[i].value, nodes[j].value)
 
 
 class ErrorAlgorithm(Exception):
+    """Error to be used for abstract classes."""
     def __str__(self):
-        return "Please implement all the methods"
+        return "Please implement all the methods."
 
 class Node:
-    
+    """Class node defines an agent."""
     def __init__(self):
         self.value = selectRandomInt(maxnodeval)
     
     def setValue(self, val):
         self.value = val
 
-class Algorithm:
-    
+class InitAlgorithm:
+    """Abstract class of initializors"""
     def execute(self):
         raise ErrorAlgorithm
     
-class AllNodesEqual(Algorithm):
-
+class AllNodesEqual(InitAlgorithm):
+    """Initializes all agents with the same value"""
     def __init__(self):
         super(AllNodesEqual, self)
     
@@ -70,8 +87,8 @@ class AllNodesEqual(Algorithm):
     def __str__(self):
         return "all nodes equal"
 
-class AllNodesDif(Algorithm):
-
+class AllNodesDif(InitAlgorithm):
+    """Initializes all agents with different values from 0 to maxnodeval"""
     def __init__(self):
         super(AllNodesDif, self)
     
@@ -87,15 +104,19 @@ class AllNodesDif(Algorithm):
         return "all nodes different"
     
 
-
-
 # MAIN #
 
 
 def main():
     
     random.seed() # initializes the seed with the current system time (default)
-    global nodes
+    global nodes 
+    global maxnodes
+    global experiments
+    global runsPerExp
+    global roundsPerRun
+    global maxnodeval
+    maxnodes, experiments, runsPerExp, roundsPerRun, maxnodeval = readvalues("specifications.txt")
     
     for i in range(maxnodes):
         nodes += [Node()]
@@ -113,9 +134,9 @@ def main():
                 j = selectRandomNode()
                 encounter(i,j)
             if (assertAlgo(maxInNetwork)):
-                print("--Experiment %d, run %d, OK" % (experiment, run))
+                print("\n--Experiment %d, run %d, OK" % (experiment, run))
             else:
-                print("--Experiment %d, run %d, FAILED" % (experiment, run))
+                print("\n--Experiment %d, run %d, FAILED" % (experiment, run))
             dumpstates()
     print("DONE.")
     
